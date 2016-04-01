153da4cd9a74faba42866ff06505cd29b48d69ae
Byron Hulcher <byronhulcher@gmail.com>
2016-04-01 12:38:16 -0400

    Now capitalizing the second word in a hyphenated phrase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 var adjs = require('corpora/data/words/adjs.json').adjs,
     fs = require("fs"),
     shuffle = require('knuth-shuffle').knuthShuffle,
-    getWords = () => { return shuffle(adjs).slice(0,20)},
-    getTitles = () => {return getWords().map((word)=>{return `JavaScript: The ${word.slice(0,1).toUpperCase()}${word.slice(1)} Parts` }); },
-    logTitles = () => {getTitles().map((title)=>{console.log(title)});},
-    writeTitles = () => {filename = "./"+Date.now()+".txt"; getTitles().map((title)=>{fs.appendFileSync(filename, title + "\n");})}
+    getWords = () => { 
+      return shuffle(adjs).slice(0,20)
+    },
+    getTitles = () => {
+      return getWords().map((word)=>{
+        return `JavaScript: The ${capitalizeWord(word)} Parts`;
+      }); 
+    },
+    capitalizeWord = (word) => { 
+      return word.split("-").map((word)=>{
+        return word.slice(0,1).toUpperCase()+word.slice(1);
+      }).join("-");
+    },
+    logTitles = (titles) => {
+      titles.map((title)=>{
+        console.log(title);
+      });
+    },
+    writeTitles = (titles) => {
+      filename = "./"+Date.now()+".txt";
+      titles.map((title)=>{
+        fs.appendFileSync(filename, title + "\n");
+      });
+    };
 
 
-writeTitles();
+logTitles(getTitles());
